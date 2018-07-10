4bb3772645572c0dc8980d0b63cdc7263a2c1e0a
Roy Segall <roy@segall.io>
2018-07-10 09:42:04 +0300

    Accept all.

--- tests/Controller/JobProcessTest.php
+++ tests/Controller/JobProcessTest.php
@@ -3,11 +3,24 @@
 namespace App\Tests\Controller;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class JobProcessTest extends TestCase {
+/**
+ * Testing the job process end point.
+ *
+ * @package App\Tests\Controller
+ */
+class JobProcessTest extends WebTestCase {
 
-  public function testJobProcessCreation() {
-    $this->assertEquals('a', 'a');
+  /**
+   * Testing the job process crud operations.
+   */
+  public function testJobProcessCrud() {
+    $client = static::createClient();
+
+    $client->request('GET', '/api/v2/job-processes');
+
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
   }
-  
+
 }
