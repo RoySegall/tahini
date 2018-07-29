34e9e8b6266d05c05f22a0095162e9bdd3c78466
Roy Segall <roy@segall.io>
2018-07-29 17:52:24 +0300

    Set the commands.

--- src/Command/UserCreateUserCommand.php
+++ src/Command/UserCreateUserCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Entity\Personal\User;
+use App\Services\TaliazUser;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,6 +16,17 @@ class UserCreateUserCommand extends Command {
 
   protected static $defaultName = 'user:create-user';
 
+  /**
+   * @var TaliazUser
+   */
+  protected $TaliazUser;
+
+  public function __construct(?string $name = null, TaliazUser $taliaz_user) {
+    parent::__construct($name);
+
+    $this->TaliazUser = $taliaz_user;
+  }
+
   protected function configure() {
     $this->setDescription('Creating a user');
   }
@@ -22,7 +35,15 @@ class UserCreateUserCommand extends Command {
     $io = new SymfonyStyle($input, $output);
     $username = $io->askQuestion(new Question('What is the username'));
     $password = $io->askQuestion(new Question('What is the password'));
+    $type = $io->askQuestion(new Question('What the type of the user - app/user'));
 
-    d([$username, $password]);
+
+    $user = new User();
+    $user->username = $username;
+    $user->setPassword($password);
+    $user->email = time() . '@example.com';
+    $user->type = $type;
+    $user->roles = [1];
+    $this->TaliazUser->createUser($user);
   }
 }
---
+++ src/Command/UserGetAuthCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class UserGetAuthCommand extends Command
+{
+  protected static $defaultName = 'user:get-auth';
+
+  protected function configure() {
+    $this->setDescription('Get the auth for a user');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $io = new SymfonyStyle($input, $output);
+    $username = $io->askQuestion(new Question('What is the username'));
+    $password = $io->askQuestion(new Question('What is the password'));
+
+    $io->success('The auth is ' . base64_encode(date("d/m/Y") . '_' . $username . '_' . $password));
+  }
+}
