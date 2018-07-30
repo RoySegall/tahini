5d0d5144d0bcc2c788f2606e456ab461bc0f4479
Roy Segall <roy@segall.io>
2018-07-30 07:37:53 +0300

    Improve the command.

--- src/Command/UserCreateUserCommand.php
+++ src/Command/UserCreateUserCommand.php
@@ -4,9 +4,11 @@ namespace App\Command;
 
 use App\Entity\Personal\User;
 use App\Services\TaliazUser;
+use App\Services\TaliazValidator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -19,10 +21,16 @@ class UserCreateUserCommand extends Command {
    */
   protected $TaliazUser;
 
-  public function __construct(?string $name = null, TaliazUser $taliaz_user) {
+  /**
+   * @var TaliazValidator
+   */
+  protected $TaliazValidator;
+
+  public function __construct(?string $name = null, TaliazUser $taliaz_user, TaliazValidator $taliaz_validator) {
     parent::__construct($name);
 
     $this->TaliazUser = $taliaz_user;
+    $this->TaliazValidator = $taliaz_validator;
   }
 
   protected function configure() {
@@ -33,8 +41,13 @@ class UserCreateUserCommand extends Command {
     $io = new SymfonyStyle($input, $output);
     $username = $io->askQuestion(new Question('What is the username'));
     $password = $io->askQuestion(new Question('What is the password'));
-    $type = $io->askQuestion(new Question('What the type of the user - app/user'));
 
+    $question = new ChoiceQuestion(
+      'What is the user type',
+      array('app' => 'Application', 'user' => 'Normal user')
+    );
+
+    $type = $this->getHelper('question')->ask($input, $output, $question);
 
     $user = new User();
     $user->username = $username;
@@ -42,6 +55,12 @@ class UserCreateUserCommand extends Command {
     $user->email = time() . '@example.com';
     $user->type = $type;
     $user->roles = [1];
+
+    if ($error = $this->TaliazValidator->validate($user)) {
+      d($error);
+      return;
+    }
+
     $this->TaliazUser->createUser($user);
   }
 }
