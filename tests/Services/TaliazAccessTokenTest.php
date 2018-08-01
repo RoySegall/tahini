3dc6eeecf3f9408c500ff928a4b8185795cbab13
Roy Segall <roy@segall.io>
2018-08-01 13:51:23 +0300

    More tests [skip ci]

--- tests/Services/TaliazAccessTokenTest.php
+++ tests/Services/TaliazAccessTokenTest.php
@@ -13,6 +13,7 @@ class TaliazAccessTokenTest extends TaliazBaseWebTestCase {
    * @throws \Exception
    */
   public function testCreateAccessToken() {
+    // Making sure the createAccessToken creates an access token.
     $user = $this->createUser(false);
     $access_token = $this->getTaliazAccessToken()->createAccessToken($user);
 
@@ -26,8 +27,9 @@ class TaliazAccessTokenTest extends TaliazBaseWebTestCase {
    * @throws \Exception
    */
   public function testGetAccessToken() {
+    // Making sure the getAccessToken returns a token.
     $user = $this->createUser(false);
-    $access_token = $this->getTaliazAccessToken()->createAccessToken($user);
+    $access_token = $this->getTaliazAccessToken()->getAccessToken($user);
 
     $this->assertNotEmpty($user->id);
     $this->assertEquals($user->id, $access_token->user->id);
@@ -37,6 +39,7 @@ class TaliazAccessTokenTest extends TaliazBaseWebTestCase {
    * @throws \Exception
    */
   public function testHasAccessToken() {
+    // Creating two users and making sure one has a token and one don't.
     $one_user = $this->createUser(false);
     $second_user = $this->createUser(true);
 
@@ -50,12 +53,32 @@ class TaliazAccessTokenTest extends TaliazBaseWebTestCase {
    * Testing the refresh token functionality.
    */
   public function testRefreshAccessToken() {
+    // Get some basic elements.
+    $user = $this->createUser(false);
+    $access_token = $this->getTaliazAccessToken()->createAccessToken($user);
+
+    // Save the ID for later.
+    $id = $access_token->id;
+
+    // Making sure the old token still exists and create a new one.
+    $this->assertNotEmpty($this->getTaliazDoctrine()->getAccessTokenRepository()->find($id));
+    $new_access_token = $this->getTaliazAccessToken()->refreshAccessToken($access_token->refresh_token);
+
+    // Checking there is a new token in town.
+    $this->assertNotEquals($new_access_token->id, $id);
+
+    // Making sure we can load the new token and not the old one.
+    $this->assertEmpty($this->getTaliazDoctrine()->getAccessTokenRepository()->find($id));
+    $this->assertNotEmpty($this->getTaliazDoctrine()->getAccessTokenRepository()->find($new_access_token->id));
   }
 
   /**
    * Testing we get the object of the user the string of the access token.
    */
   public function testLoadByAccessToken() {
+    $user = $this->createUser(false);
+    $access_token = $this->getTaliazAccessToken()->createAccessToken($user);
+    $this->assertEquals($access_token, $this->getTaliazAccessToken()->loadByAccessToken($access_token->access_token));
   }
 
   /**
